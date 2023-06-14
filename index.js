80deff412b8252d7fd74aa550c49d0c52dafa92c
WinterJackson <winterjacksonwj@gmail.com>
2023-06-14 11:54:44 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+var multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 100); 
+
+let num3 = 24 ;
+let num4 = 5 ;
+let mod = num3 % num4 ;
+
+let max = Math.max(1,12,3,19,4,15,20) ;
